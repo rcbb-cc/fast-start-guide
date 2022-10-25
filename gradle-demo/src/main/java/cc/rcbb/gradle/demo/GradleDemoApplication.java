8ba99f369140b466682f0748ee4f9cc61481fe48
rcbb.cc <www.rcbb.cc@gmail.com>
2022-10-25 21:04:55 +0800

    feature：新增gradle-demo

---
+++ gradle-demo/src/main/java/cc/rcbb/gradle/demo/GradleDemoApplication.java
@@ -0,0 +1,13 @@
+package cc.rcbb.gradle.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleDemoApplication.class, args);
+	}
+
+}
---
+++ gradle-demo/src/test/java/cc/rcbb/gradle/demo/GradleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.rcbb.gradle.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
