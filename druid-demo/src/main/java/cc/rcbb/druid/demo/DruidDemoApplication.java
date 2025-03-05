af425259bb2a6a60aa4d33f5a99e55a031b81268
rcbb.cc <www.rcbb.cc@gmail.com>
2025-03-05 15:19:53 +0800

    feature：新增druid-demo

---
+++ druid-demo/src/main/java/cc/rcbb/druid/demo/DruidDemoApplication.java
@@ -0,0 +1,13 @@
+package cc.rcbb.druid.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidDemoApplication.class, args);
+    }
+
+}
---
+++ druid-demo/src/test/java/cc/rcbb/druid/demo/DruidDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.rcbb.druid.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DruidDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
